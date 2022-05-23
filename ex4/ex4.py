cb38c422e7f0acd0fe221813be1193fd88c62013
kim <kimkes25@gmail.com>
2022-05-23 17:10:39 +0300

    j

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -4,7 +4,10 @@ import math
 import random
 import numpy as np
 import scipy as sp
+from scipy.linalg import lstsq
 import random
+from collections import defaultdict
+
 
 
 # helpers
@@ -15,6 +18,7 @@ def find_k_nearest_indexes(X, u, k):
 
 
 def knn(X, y, u, K):
+    # Expecting y to be with shape == (n,1)
     k_nearest_idx = find_k_nearest_indexes(X, u, K)
     return round(1 / K * y[k_nearest_idx, :].sum())
 
@@ -28,7 +32,7 @@ def randonly_divide_idx(X, L):
 
 
 def L_fold_knn(L, K, X, y):
-    # print("X: ", X)
+    # Expecting y to be with shape == (n,1)
     n = X.shape[0]
     groups_idx = randonly_divide_idx(X, L)
     cnt = 0
@@ -41,21 +45,81 @@ def L_fold_knn(L, K, X, y):
         # check if knn result is like the real result in y for each item in current group
         for idx in current_group_idx:
             res = knn(new_X, new_y, X[idx], K)
-            # TODO fix this
             if res == y[idx]:
                 cnt += 1
     return cnt / n
 
 
-def lin_reg(X, y):
-    pass
+def calc_ssx(b,X,y,is_ssr=False, is_sst=False):
+    curr_sum = 0
+    for i in range(X.shape[1]):
+        y_avg = y.mean()
+        y_original = y[i]
+        y_predicted = np.dot(X, b)
+        if is_ssr:
+            return sum(pow( y_predicted - y_avg , 2))
+        elif is_sst:
+            return sum(pow(y - y_avg , 2))
+    return curr_sum
 
+
+def lin_reg(X, y):
+    # Expecting y to be with shape == (n,1)
+    ones = np.ones(shape=(X.shape[0], 1), dtype=np.int64)
+    X = np.c_[X, ones]
+    # X = np.matrix(np.array(ones,X)).transpose()
+    b = lstsq(X, y)[0]
+    ssr = calc_ssx(b,X,y,is_ssr=True)
+    sst = calc_ssx(b,X,y,is_sst=True)
+    return b , ssr/sst
 
 # you may change the function signature and add parameters (with or without default values)
-def my_procedue():
-    pass
+def my_procedue(ge_path, age_path, bmi_path, gender_path, gene_names_path):
+    X = load_txt_as_numpy(ge_path)
+    age = load_txt_as_numpy(age_path).reshape((-1, 1))
+    bmi = load_txt_as_numpy(bmi_path).reshape((-1, 1))
+    gender = load_txt_as_numpy(gender_path).reshape((-1, 1))
+    ABG = np.c_[age,bmi,gender]
+    n = X.shape[0]
+    gene_to_num_dict = defaultdict(int)
+    line_num_to_gene_name_dict  = line_num_to_gene_name(gene_names_path)
 
+    for i in range(100):
+        # Preparing data
+        indexes = random.sample(range(n), int(n / 2))
+        new_X = X[indexes]
+        new_ABG = ABG[indexes]
+        # print(f"----Iteration number {i}-----\n")
+        best_gene, max_gene_index, max_r_power_2 = one_iteration(new_X, new_ABG, line_num_to_gene_name_dict)
+        # print(f"Winner gene with highst R^2 is {best_gene}\n {max_r_power_2}")
+        gene_to_num_dict[best_gene]+=1
+    return sorted(((v, k) for k, v in gene_to_num_dict.items()), reverse=True)[:1]
+
+def load_txt_as_numpy(path):
+    return np.loadtxt(path)
+
+def one_iteration(X, ABG, line_num_to_gene_name_dict):
+    max_r_power_2 = float("-inf")
+    max_gene_index = 0
+    for j in range(X.shape[1]): #for col in X - for gene
+        X_j = X[:,j]
+        b, r_power_2 = lin_reg(ABG ,X_j)
+        if r_power_2 > max_r_power_2:
+            max_r_power_2 = r_power_2
+            max_gene_index = j
+    best_gene = line_num_to_gene_name_dict[max_gene_index]
+    return best_gene, max_gene_index, max_r_power_2
+
+def line_num_to_gene_name(file_name):
+    d = {}
+    curr_line = 0
+    with open(file_name, 'r') as read_obj:
+        for line_read in read_obj:
+            gene_read = "".join(line_read.split())
+            d[curr_line] = gene_read
+            curr_line += 1
+    return d
 
 if __name__ == '__main__':
-    # you can write whatever you want here
+    print(my_procedue("ge.txt","age.txt","bmi.txt","gender.txt","gene_names.txt"))
     pass
--- ex4/test.py
+++ ex4/test.py
@@ -7,6 +7,7 @@ from ex4 import *
 import os
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import itertools
 
 
 def _pickle(fp, object="", is_load=False, is_dump=False):
@@ -33,7 +34,9 @@ def write_to_file(file, content, is_log=False):
 
 
 def test_a():
-    X = np.array([[1, 1, 1], [4, 4, 4]])
+    X = np.array([
+        [1, 1, 1],
+        [4, 4, 4]])
     y = np.array([
         [1],
         [0],
@@ -73,9 +76,7 @@ def load_txt_as_numpy(path):
     return np.loadtxt(path)
 
 
-def test_d(X):
-    y = load_txt_as_numpy("caco.txt")
-    y = y.reshape((-1, 1))
+def test_d(X,y):
     L = 10
     x_lst = []
     y_lst = []
@@ -87,22 +88,50 @@ def test_d(X):
 
 
 def plot_fig_d(x, y, run_kind):
-    plt.scatter(x, y)
-    plt.title(f"Accuracy per k value {run_kind}")
+    plt.plot(x, y,label=run_kind)
+    plt.title(f"Results d")
     plt.xlabel("K value")
     plt.ylabel("Accuracy")
-    plt.savefig(f"{run_kind}.png")
+    plt.legend(loc="upper left")
+    plt.savefig(f"Results.png")
 
 
 def runs_d():
     ### Using all the features in the data
     X = load_txt_as_numpy("ge.txt")
-    x, y = test_d(X)
-    plot_fig_d(x, y, "Using all the features in the data")
+    y = load_txt_as_numpy("caco.txt")
+    y = y.reshape((-1, 1))
+    x_res, y_res = test_d(X,y)
+    plot_fig_d(x_res, y_res, "only ge.txt")
+
     ### Using only the subset of genes that you found in section (c)
+    paths = ["KEGG_WNT_SIGNALING_PATHWAY", "KEGG_LYSOSOME","KEGG_PPAR_SIGNALING_PATHWAY"]
+    ## geting the relavent cols for sets chosen
+    set_name_to_genes_list = create_set_name_to_genes_list_dict()
+    gene_names_file_name_to_line_dict = gene_names_file_name_to_line_nums()
+    name_to_idx = create_set_name_to_idx_gene_listdict(set_name_to_genes_list, gene_names_file_name_to_line_dict)
+    relavent_ids_lists = [name_to_idx[path] for path in paths]
+    relavent_cols_3_sets = []
+    for listt in relavent_ids_lists:
+        for item in listt:
+            relavent_cols_3_sets.append(item)
+    B = load_txt_as_numpy("ge.txt")
+    y = load_txt_as_numpy("caco.txt")
+    y = y.reshape((-1, 1))
+    B = B[:,relavent_cols_3_sets]
+    x_res, y_res = test_d(B,y)
+    plot_fig_d(x_res, y_res, "3 pathways")
+
     ### Using only the subset of genes and age, gender and BMI as additional features
-
-
+    C = load_txt_as_numpy("ge.txt")
+    C = C[:,relavent_cols_3_sets]
+    y = load_txt_as_numpy("caco.txt").reshape((-1, 1))
+    age = load_txt_as_numpy("age.txt").reshape((-1, 1))
+    bmi = load_txt_as_numpy("bmi.txt").reshape((-1, 1))
+    gender = load_txt_as_numpy("gender.txt").reshape((-1, 1))
+    C = np.c_[C, age,bmi,gender]
+    x_res, y_res = test_d(C,y)
+    plot_fig_d(x_res, y_res, "3 pathways +  3 files")
 ################################################################################################## C #############################################################################################
 def create_set_name_to_genes_list_dict():
     f = open("kegg.MSigDB.symbols.gmt", "r")
@@ -112,8 +141,6 @@ def create_set_name_to_genes_list_dict():
         name = "".join(line_splited[0].split())
         set_name_to_genes_list_dict[name] = line_splited[2:]
     return set_name_to_genes_list_dict
-
-
 def gene_names_file_name_to_line_nums():
     file_name = "gene_names.txt"
     d = {}
@@ -125,26 +152,14 @@ def gene_names_file_name_to_line_nums():
             curr_line += 1
     return d
 
-
 def create_set_name_to_idx_gene_listdict(set_name_to_genes_list_dict, names_file_name_to_line_nums):
     set_name_to_idx_gene_list = defaultdict(list)
-    genes_from_keg = set()
-    genes_missing = set()
+    set_genes_percent_dict = defaultdict(int)
     for gene_set in set_name_to_genes_list_dict.keys():
         for gene in set_name_to_genes_list_dict[gene_set]:
-            genes_from_keg.add(gene)
             if gene in names_file_name_to_line_nums:
                 set_name_to_idx_gene_list[gene_set].append(names_file_name_to_line_nums[gene])
-            else:
-                genes_missing.add(gene)
-    print("genes_from_keg: \n", genes_from_keg)
-    print("genes_missing_set: \n", genes_missing)
-    print(f"per of genes not apearing is {len(genes_missing) / len(genes_from_keg)}")
-    _pickle("genes_missing_set.dat", genes_missing, is_dump=True)
-    write_to_file("genes_missing_set.txt", genes_missing)
     return set_name_to_idx_gene_list
-
-
 def check_if_string_in_file(file_name, string_to_search):
     """ Check if any line in the file contains given string """
     # Open the file in read only mode
@@ -157,16 +172,90 @@ def check_if_string_in_file(file_name, string_to_search):
     return False
 
 
-def search_for_3_best_sets():
+def search_for_3_best_sets(k):
+    X = load_txt_as_numpy("ge.txt")
+    y = load_txt_as_numpy("caco.txt")
+    y = y.reshape((-1, 1))
     set_name_to_genes_list = create_set_name_to_genes_list_dict()
     gene_names_file_name_to_line_dict = gene_names_file_name_to_line_nums()
     name_to_idx = create_set_name_to_idx_gene_listdict(set_name_to_genes_list, gene_names_file_name_to_line_dict)
+    set_name_to_accuracy = {}
+    for set,idx in name_to_idx.items():
+        # take only certain cols
+        res = L_fold_knn(L=10,K=k,X=X[:,idx],y=y)
+        set_name_to_accuracy[set] = res
+    return set_name_to_accuracy
+
+def test_e():
+    X = np.array([
+        [0],
+        [1],
+        [2],
+
+    ])
+    y = np.array([
+        [0],
+        [1],
+        [2],
+    ])
+
+    X1 = np.array([
+        [0, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2],
+        [3, 3, 3],
+        [4, 4, 4],
+        [5, 5, 5],
+
+    ])
+    y1 = np.array([
+        [0],
+        [0],
+        [0],
+        [1],
+        [1],
+        [1]
+    ])
+
+    print(lin_reg(X,y))
+
+def line_num_to_gene_name():
+    file_name = "gene_names.txt"
+    d = {}
+    curr_line = 0
+    with open(file_name, 'r') as read_obj:
+        for line_read in read_obj:
+            gene_read = "".join(line_read.split())
+            d[curr_line] = gene_read
+            curr_line += 1
+    return d
+
+def test_f():
+    X = load_txt_as_numpy("ge.txt")
+    age = load_txt_as_numpy("age.txt").reshape((-1, 1))
+    bmi = load_txt_as_numpy("bmi.txt").reshape((-1, 1))
+    gender = load_txt_as_numpy("gender.txt").reshape((-1, 1))
+    ABG = np.c_[age,bmi,gender]
+    max_r_power_2 = float("-inf")
+    max_gene_index = 0
+    for j in range(X.shape[1]): #for col in X - for gene
+        X_j = X[:,j]
+        b, r_power_2 = lin_reg(ABG,X_j)
+        if r_power_2 > max_r_power_2:
+            max_r_power_2 = r_power_2
+            max_gene_index = j
+    d  = line_num_to_gene_name()
+    best_gene = d[max_gene_index]
+    return best_gene, max_gene_index, max_r_power_2
 
 
 if __name__ == '__main__':
     # you can write whatever you want here
     # test_a()
     # test_b()
-    runs_d()
-    # search_for_3_best_sets()
+    # runs_d()
+    # for k in range(1,10):
+    #     d=  search_for_3_best_sets(k)
+    #     print(sorted(((v, k) for k, v in d.items()), reverse=True)[:4])
     # pprint(_pickle("genes_missing_set.dat", is_load=True))
+    print(test_f())
